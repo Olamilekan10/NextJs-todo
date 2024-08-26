c3beb309c20bddab4417a9f940cc26946fbc42f5
issaabubakry <issaabubakry@gmail.com>
2024-08-26 11:50:38 -0700

    created the design of the todo with tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-'use client';
+// 'use client';
 import { useState } from 'react';
 
-export default function Home() {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
-  const [isEditing, setIsEditing] = useState(false);
-  const [currentTask, setCurrentTask] = useState(null);
+// export default function Home() {
+//   const [tasks, setTasks] = useState([]);
+//   const [newTask, setNewTask] = useState('');
+//   const [isEditing, setIsEditing] = useState(false);
+//   const [currentTask, setCurrentTask] = useState(null);
 
   const addTask = () => {
     if (!newTask.trim()) return;
@@ -62,7 +62,7 @@ export default function Home() {
               <div>
                 <button
                   onClick={() => editTask(task)}
-                  className="text-blue-500 hover:text-blue-700 mx-2"
+                  className="text-blue-500 hover:text-blue-700"
                 >
                   Edit
                 </button>
